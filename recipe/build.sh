0c23f7fd340a3eb8e12a380dd1d2107634943e81
Hadrien Mary <hadrien.mary@gmail.com>
2023-12-05 21:07:08 -0500

    add cudnn and cusparse

--- recipe/build.sh
+++ recipe/build.sh
@@ -51,6 +51,6 @@ export EXTERNAL_PHMAP_INCLUDE_DIR="${BUILD_PREFIX}/include/"
 export EXTERNAL_CUTLASS_INCLUDE_DIR="${BUILD_PREFIX}/include/"
 
 # note that we patch PYG_CMAKE_ARGS into setup.py, in order to set Torch_DIR correctly also for cross-compilation
-export PYG_CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DTorch_DIR=${PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake/Torch"
+export PYG_CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DTorch_DIR=${PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake/Torch -DUSE_CUDNN=1 -DUSE_CUSPARSELT=1"
 
 ${PYTHON} -m pip install . -vvv
